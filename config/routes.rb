ef5f7283c94ca7c75bed1db1b6b9e79fd0846b37
evelyn gomez <evelyngomezfrez@gmail.com>
2021-03-26 17:23:26 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
+  root 'pages#one'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
